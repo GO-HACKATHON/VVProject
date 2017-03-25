821a23c72382ef11911d9d071add058fa27bfc62
cync48 <ridhoperdana@ymail.com>
2017-03-25 15:02:14 +0700

    konfigurasi mongodb laravel selesai

---
+++ VVProject/app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class MainController extends Controller
+{
+    public function cobamongo()
+    {
+        $users = DB::collection('smartphone')->where('Title', 'like', '%google%')->get();;
+        return $users;
+    }
+}
