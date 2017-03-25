2b2ea5249cae0c5f37c0db765b658c5e586f8a36
Ronald Gunawan R <geronald10@gmail.com>
2017-03-25 14:30:56 +0700

    Add recommendation list result page + custom css + partial resources view

---
+++ VVProject/resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html class="no-js">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>VV - Recommendation List - Smartphone Auto Recommendation</title>
+
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+		<link rel="shortcut icon" href="favicon.ico">
+		<!-- Google Webfonts -->
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+		
+		<!-- Animate.css -->
+		<link rel="stylesheet" href="{{ URL::to('/assets/hydrogen/css/animate.css') }}">
+		<!-- Icomoon Icon Fonts-->
+		<link rel="stylesheet" href="{{ URL::to('/assets/hydrogen/css/icomoon.css') }}">
+		<!-- Magnific Popup -->
+		<link rel="stylesheet" href="{{ URL::to('/assets/hydrogen/css/magnific-popup.css') }}">
+		<!-- Salvattore -->
+		<link rel="stylesheet" href="{{ URL::to('/assets/hydrogen/css/salvattore.css') }}">
+		<!-- Theme Style -->
+		<link rel="stylesheet" href="{{ URL::to('/assets/hydrogen/css/style.css') }}">
+		<!-- Modernizr JS -->
+		<script src="{{ URL::to('assets/hydrogen/js/modernizr-2.6.2.min.js') }}"></script>
+	</head>
+	<body>
---
+++ VVProject/resources/views/recommendation-list.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="fh5co-main">
+	<div class="container">
+		<div class="row">
+			<h3 class="row-title">Recommendation Result:</h3>
+			<div class="col-md-12 label-caption">Categories :
+				<span class="label label-primary-caption">Video and Photography</span>
+				<span class="label label-primary-caption">Games</span>
+				<span class="label label-primary-caption">Utilities</span>
+			</div>
+
+		    <div id="fh5co-board" data-columns>
+		    	<div class="item">
+		    		<a href="{{ url('detail') }}">
+		        		<div class="animate-box">
+			        		<img src="{{ URL::to('/dummy-data/apple-iphone-7.jpg') }}" alt="filtered smartphone">
+		        		</div>
+		    			<div class="fh5co-desc">Apple iPhone 7 - 128 GB</div>
+					</a>
+		    	</div>
+		    	<div class="item">
+		 			<a href="about.html">
+		        		<div class="animate-box">
+		        			<img src="{{ URL::to('/dummy-data/samsung-galaxy-c5.jpg') }}" alt="filtered smartphone">
+		        		</div>
+		        		<div class="fh5co-desc">Lenovo A7010 Hero</div>
+		    		</a>
+		    	</div>
+		    </div>
+	    </div>
+   </div>
+</div>
+@endsection
