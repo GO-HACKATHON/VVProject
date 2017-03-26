002a8f81c8b212e7f0fefe1a04fcd9283fb6edc3
Ronald Gunawan R <geronald10@gmail.com>
2017-03-26 08:52:29 +0700

    Add Form Search for Landing Page

--- VVProject/resources/views/index.blade.php
+++ VVProject/resources/views/index.blade.php
@@ -1,50 +1,51 @@
 @extends('layouts.main')
 
 @section('content')
-<form class="form-horizontal" action="{{URL::to('form')}}"" method="POST">
-          
-          <div class="modal-body">
-                <h4> Categories </h4>
-                {{ csrf_field() }}
-              <div class="form-group">
-                <div class="col-sm-offset-0 col-sm-4">
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox" name="game" value="true"> GAME
-                    </label>
-                  </div>
-                </div>
-                <div class="col-sm-4">
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox" name="photography" value="true"> PHOTOGRAPHY
-                    </label>
-                  </div>
-                </div>
-                <div class="col-sm-4">
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox" name="utility" value="true"> UTILITY
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <hr>
-              <div class="form-group">
-                <div class="col-sm-offset-0 col-sm-12">
-                    <label>Additional Request</label>
-                    <textarea class="form-control" rows="3" name="description"></textarea>
-                </div>
-              </div>
-              <div class="price">
-              	<input type="text" name="price">
-              </div>
-
+<div class="logo-vv">
+  <h1>VV</h1>
+  <p>Smartphone Auto Recommendation</p>
+</div>
+<form class="form-horizontal form-margin" action="{{ URL::to('form') }}"" method="POST">
+  <div class="container col-md-6 col-md-offset-3 form-sheet">
+      <div class="form-group">
+       <div class="col-sm-12">
+        <label>Categories</label>
+       </div>
+        {{ csrf_field() }}
+        <div class="col-sm-4">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" name="game" value="true"> GAME
+            </label>
           </div>
-          <div class="modal-footer">
-                  <button href="recommendation-list.blade.php" type="submit" class="btn btn-default">Search</button>
-            
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+        <div class="col-sm-4">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" name="photography" value="true"> PHOTOGRAPHY
+            </label>
           </div>
-          </form>
+        </div>
+        <div class="col-sm-4">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" name="utility" value="true"> UTILITY
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-12">
+            <label>Additional Request</label>
+            <textarea class="form-control" rows="3" name="description"></textarea>
+        </div>
+      </div>
+      <div class="price">
+        <label>Enter your preferable price here -></label>
+      	<input class="form-control" type="text" name="price">
+      </div>
+      <hr>
+      <button href="recommendation-list.blade.php" type="submit" class="btn btn-default btn-lg center btn-center">Search</button>
+  </div>
+</form>
 @endsection
